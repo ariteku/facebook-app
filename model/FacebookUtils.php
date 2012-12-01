ee367c3eaf1aad18db2f2a16560ec6ab536c74a3
takuan <ariteku@gmail.com>
2012-12-02 03:08:54 +0900

    fix

--- model/FacebookUtils.php
+++ model/FacebookUtils.php
@@ -5,7 +5,6 @@
 //  Facebook APIを処理するクラス
 //
 //  Created by takuan on 12/05/13.
-//  Copyright (c) 2012年 takuan. All rights reserved.
 //
 include __DIR__.'/../vendor/php-sdk/src/facebook.php';
 include __DIR__.'/ApplicationConstants.php';
@@ -29,7 +28,7 @@ class FacebookUtils {
 	/**
 	 * ログインチェック
 	 *
-	 * @author 046
+	 * @author takuan
 	 */
 	public function checkLogin() {
 		$user = $this->facebook->getUser();
@@ -51,7 +50,7 @@ class FacebookUtils {
 	/**
 	 * ページがイイねされているかどうか判定する
 	 * 
-	 * @author 046
+	 * @author takuan
 	 * @return イイねされていれば1を返す
 	 */
 	public function checkLiked() {
@@ -62,7 +61,7 @@ class FacebookUtils {
 	/**
 	 * ログイン中のユーザーにPhotoに画像を投稿
 	 *
-	 * @author 046
+	 * @author takuan
 	 */
 	public function writePhotoMe($writeStr, $photoUrl) {
 		try {
